1942483fbbcf9d32b43d6d5cb7dcfed542abe0ef
Graham Swanson <graham.swanson@swansondesigns.com>
2020-03-30 19:02:19 -0600

    Test commit 1

--- themes/dzgala-child/header-home.php
+++ themes/dzgala-child/header-home.php
@@ -17,9 +17,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link href="https://fonts.googleapis.com/css?family=Sanchez" rel="stylesheet">
-
 	<?php wp_head(); ?>
-
 </head>
 
 <body <?php body_class( 'no-js' ); ?>>
